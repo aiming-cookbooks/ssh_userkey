71970bf46dd7828c0626edb051c7370923f536c4
holy <holysugar@gmail.com>
2013-11-27 14:09:36 +0900

    notifies when updated

--- providers/default.rb
+++ providers/default.rb
@@ -5,23 +5,29 @@ action :create do
   seckey   = "#{home}/.ssh/id_rsa"
   pubkey   = "#{seckey}.pub"
 
-  directory "#{home}/.ssh" do
-    mode  0700
-    owner username
-    group username
-  end
+  unless ::File.exist?(pubkey)
 
-  execute "generate ssh keys for #{username}" do
-    user username
-    creates pubkey
-    command "ssh-keygen -t rsa -q -f #{seckey} -P \"\""
-    notifies :run, "ruby_block[print-public-key]"
-  end
-
-  ruby_block "print-public-key" do
-    block do
-      Chef::Log.info ::File.read(pubkey)
+    directory "#{home}/.ssh" do
+      mode  0700
+      owner username
+      group username
     end
-    action :nothing
+
+    execute "generate ssh keys for #{username}" do
+      user username
+      creates pubkey
+      command "ssh-keygen -t rsa -q -f #{seckey} -P \"\""
+      notifies :run, "ruby_block[print-public-key]"
+    end
+
+    ruby_block "print-public-key" do
+      block do
+        Chef::Log.info ::File.read(pubkey)
+      end
+      action :nothing
+    end
+
+    new_resource.updated_by_last_action(true) # notify when updated
+
   end
 end
